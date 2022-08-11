801d23f98ff643eeac5e80b00eae098808892cae
Robson Melo de Souza <sys.robson@gmail.com>
2022-08-11 17:02:39 -0300

    added some styling and fetching data from the API

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {useQuery} from '@tanstack/react-query'
 import './Posts.css'
+import Moment from 'moment';
 
 function Posts() {
 
 
     const { isLoading, error, data } = useQuery(['repoData'], () =>
-    fetch('https://newsapi.org/v2/everything?q=florida&from=2022-07-11&sortBy=publishedAt&apiKey='+process.env.REACT_APP_API_KEY).then(res =>
+    fetch('https://newsapi.org/v2/everything?q=flowers&from=2022-07-11&sortBy=publishedAt&apiKey='+process.env.REACT_APP_API_KEY).then(res =>
       res.json()
     )
   )
@@ -22,7 +23,15 @@ if (error){
   return (
     <div className='posts'>
         {data.articles.map((post) =>(
-            <div className='post__card'> <a href={post.url} className='post__link'>{post.title} </a></div>
+            
+            <div className='post'>
+                <img src={post.urlToImage} alt={post.title} className='post__img'/>
+                <p className='post__source'>{post.source.name}</p>
+                <p className='post__title'>{post.title}</p>
+                <p className='post__date'>{post.publishedAt}</p>
+                <a href={post.url} className='post__link'>Read article</a>
+
+            </div>
         ))}
 
     </div>
