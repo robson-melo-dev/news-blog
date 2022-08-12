fa44f714f249471968d15886ed6ea55c6be6797a
Robson Melo de Souza <sys.robson@gmail.com>
2022-08-12 11:20:59 -0300

    added pagination, not fully functional yet

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useQuery} from '@tanstack/react-query'
 import './Posts.css'
 import Moment from 'moment';
 
 function Posts() {
+const [currentPage, setCurrentPage] = useState(1)
+const postsPerPage = 9
+const [pages, setPages] = useState([])
 
-
-    const { isLoading, error, data } = useQuery(['repoData'], () =>
-    fetch('https://newsapi.org/v2/everything?q=flowers&from=2022-07-11&sortBy=publishedAt&apiKey='+process.env.REACT_APP_API_KEY).then(res =>
-      res.json()
-    )
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    fetch(
+      'https://newsapi.org/v2/everything?pageSize='+postsPerPage+'&q=florida&from=2022-07-11&sortBy=publishedAt&page='+currentPage+'&apiKey=' +
+        process.env.REACT_APP_API_KEY
+    ).then(res => {
+      if (res.ok) return res.json()
+      else throw Error(res.status + "")
+    })
   )
     
 if (isLoading){
-    return <div>Loading...</div>
+    return <h1>Loading...</h1>
 }
 
 if (error){
-    return <div>Error: {error.message}</div>
+    return <h1>Error: {error.message}</h1>
+}
+
+console.log("current page ",currentPage)
+
+if(pages.length === 0){
+    for (let i=1; i<=Math.ceil(data.totalResults/postsPerPage);i++){
+        if(i<=10){
+            pages.push(i)
+        }        
+    }
+}
+
+
+
+function paginate(e){
+    setCurrentPage(e.target.id)    
 }
 
   return (
+    <div className='postsTable'>
     <div className='posts'>
-        {data.articles.map((post) =>(
+        {data.articles?.map?.((post) =>(
             
             <div className='post'>
                 <img src={post.urlToImage} alt={post.title} className='post__img'/>
@@ -30,10 +53,19 @@ if (error){
                 <p className='post__title'>{post.title}</p>
                 <p className='post__date'>{post.publishedAt}</p>
                 <a href={post.url} className='post__link'>Read article</a>
-
             </div>
         ))}
-
+        
+    </div>
+    <footer className='pages'>
+    page <ul className='pages__numbers'> 
+            {pages?.map?.((number)=>(
+                <li key={number} className='page' >
+                    <a href='' id={number} onClick={paginate} className='page__link'>{number}</a>
+                </li>
+            ))}
+        </ul>
+    </footer>
     </div>
   )
 }
